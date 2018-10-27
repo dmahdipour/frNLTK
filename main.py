d56dafa6812bbdbdb3951404d4f58177a6b87184
dmy <daruosh.mehdipour@gmail.com>
2018-10-28 00:24:49 +0330

    Modify main.py for adding import and use fileReader and fileWriter

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import normalizer
 import tokenizer
 import tokenCounter
 import dicSorter
+import fileWriter
+import fileReader
 
 
 myCorpusPath = "ahrarnews_corpus/"
+myTokenizedPath = "tokenized/"
 
 
 txtFileList = glob.glob(f'{myCorpusPath}*.txt')
@@ -21,4 +24,6 @@ filesWordNormalize = normalizer.my_normalizing(filesContent)
 filesWordTokenize = tokenizer.my_word_tokenize(filesWordNormalize)
 filesTokenCount = tokenCounter.my_token_conter(filesWordTokenize)
 filesSorted_dic = dicSorter.dic_key_sort(filesTokenCount, 0)
+fileWriter.my_file_writer(myTokenizedPath+"0_myIndexList.tok", filesSorted_dic)
+
 
