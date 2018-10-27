19eec86a466d917cb7f223ffcb73434259e1b5ff
dmy <daruosh.mehdipour@gmail.com>
2018-10-28 00:02:25 +0330

    Modify main.py for adding import and use normalizer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import glob
 from tqdm import tqdm
 
+import normalizer
+
 
 myCorpusPath = "ahrarnews_corpus/"
 
@@ -11,4 +13,5 @@ txtFileList = glob.glob(f'{myCorpusPath}*.txt')
 filesContent = ""
 for filesNum in tqdm(range(len(txtFileList))):
     filesContent = filesContent + fileReader.my_file_reader(txtFileList[filesNum], "UTF-8") + "\n"  
-    
+
+filesWordNormalize = normalizer.my_normalizing(filesContent)
