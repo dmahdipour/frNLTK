0d671e55a46822abf5c0b77a9694ba64633ca420
dmy <daruosh.mehdipour@gmail.com>
2018-10-28 13:18:58 +0330

    Modify fileReader.py to add new def named my_floatFile_reader

--- fileReader.py
+++ fileReader.py
@@ -1,3 +1,9 @@
 def my_file_reader(myFile, fileEncode):
     fileContent = open(myFile, encoding = fileEncode)
     return fileContent.read()
+
+
+def my_floatFile_reader(myFile):
+    fileContent = open(myFile, 'r')
+    x = [float(line.strip()) for line in fileContent if line]    
+    return x
