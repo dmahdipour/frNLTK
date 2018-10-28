ce537d26e21d320c5a5baf1a805c2603558f5050
dmy <daruosh.mehdipour@gmail.com>
2018-10-28 13:42:55 +0330

    Modify tf_idf_dicCreator.py file to save unsaved new def

--- tf_idf_dicCreator.py
+++ tf_idf_dicCreator.py
@@ -1,11 +1,4 @@
-def my_TF_IDF(mainDic, axDic):
-    tmpDic = {}
-    for i in mainDic:    
-        if i in axDic:
-            tmpDic[i] = axDic.get(i)/mainDic.get(i)
-        else:
-            tmpDic[i] = 0
-    return tmpDic
+import numpy as np
 
 
 def my_TF(mainDic, axDic):
@@ -16,3 +9,21 @@ def my_TF(mainDic, axDic):
         else:
             tmpDic[i] = 0
     return tmpDic
+
+def my_DF(mainDic):
+    [row, col] = (mainDic.shape)
+    df = np.zeros(col).astype(int)    
+    for i in range(col):
+        for j in range(1, row):
+            if int(mainDic[j, i]) > 0:
+                df[i] = df[i]+1
+    return df 
+
+
+def my_TF_IDF(mainDic, axDic):
+    mainDic = np.array(mainDic).astype(float)
+    [row, col] = (mainDic.shape)
+    for i in range(row):
+        mainDic[i,:] = mainDic[i,:]/axDic
+    
+    return mainDic[1:row,:]
