70378302d98a1296907643d7367efc8a562022d0
dmy <daruosh.mehdipour@gmail.com>
2018-10-28 13:30:07 +0330

    Modify vectorSimilarity.py to add new def named cos_sim2_vectors

--- vectorSimilarity.py
+++ vectorSimilarity.py
@@ -9,3 +9,10 @@ def cos_sim_vectors(doc1, doc2):
     ls_d2 = list(d2.values())
     
     return dot(ls_d1, ls_d2)/(norm(ls_d1)*norm(ls_d2))
+
+
+def cos_sim2_vectors(doc1, doc2):
+    ls_d1 = fileReader.my_floatFile_reader(doc1)
+    ls_d2 = fileReader.my_floatFile_reader(doc2)
+    
+    return dot(ls_d1, ls_d2)/(norm(ls_d1)*norm(ls_d2))
