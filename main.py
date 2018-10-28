4bec1e801f9f4a861f24aed1c6fbcd48b3191c36
dmy <daruosh.mehdipour@gmail.com>
2018-10-28 12:59:25 +0330

    Modify main.py for adding import and useNTCS for reduction in code lines

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import fileWriter
 import fileReader
 import tf_idf_dicCreator
 import vectorSimilarity
+import NTCS
 
 
 myCorpusPath = "ahrarnews_corpus/"
@@ -24,20 +25,14 @@ filesContent = ""
 for filesNum in tqdm(range(len(txtFileList))):
     filesContent = filesContent + fileReader.my_file_reader(txtFileList[filesNum], "UTF-8") + "\n"  
 
-filesWordNormalize = normalizer.my_normalizing(filesContent)
-filesWordTokenize = tokenizer.my_word_tokenize(filesWordNormalize)
-filesTokenCount = tokenCounter.my_token_conter(filesWordTokenize)
-filesSorted_dic = dicSorter.dic_key_sort(filesTokenCount, 0)
+corpusVocabC = NTCS.n_t_c_s(corpusContent)
 fileWriter.my_file_writer(myTokenizedPath+"0_myIndexList.tok", filesSorted_dic)
 
 # one_By_One
 for fileNum in tqdm(range(len(txtFileList))):    
     fileContent = fileReader.my_file_reader(txtFileList[fileNum], "UTF-8")
     
-    wordNormalize = normalizer.my_normalizing(fileContent)
-    wordTokenize = tokenizer.my_word_tokenize(wordNormalize)
-    tokenCount = tokenCounter.my_token_conter(wordTokenize)    
-    sorted_dic = dicSorter.dic_key_sort(tokenCount, 0)
+    sorted_dic = NTCS.n_t_c_s(fileContent)
    
     fileName = txtFileList[fileNum].split(myCorpusPath)
     fileName = fileName[1].split(".txt")
