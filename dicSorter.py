c4adf6ffa159caa4cf24840a1d4f1f08a95a822d
dmy <daruosh.mehdipour@gmail.com>
2018-11-04 23:18:13 +0330

    Modify dicSorter.py file to optimize it.

--- dicSorter.py
+++ dicSorter.py
@@ -1,6 +1,4 @@
-import operator
 from operator import itemgetter
-from collections import OrderedDict
 
 def dic_key_sort(dic, idx):
-    return dict(OrderedDict(sorted(dic.items(), key=itemgetter(idx))))
+    return dict(sorted(dic.items(), key=itemgetter(idx), reverse=True))
