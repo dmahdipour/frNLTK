3517b0ebae0778d9bb6a39e83ffedda07a674f54
dmy <daruosh.mehdipour@gmail.com>
2018-11-12 16:28:46 +0330

    Modify NTCS.py file to add and use stopWordRemoval definitions.

--- NTCS.py
+++ NTCS.py
@@ -2,13 +2,13 @@ import normalizer
 import tokenizer
 import tokenCounter
 import dicSorter
-import stopwordRemover
+#import stopwordRemover
 
 
 def n_t_c_s(fileContent):
-    fileContent = normalizer.my_tiny_normalizing(fileContent)
-    fileContent = stopwordRemover.remove_proNone(fileContent)
-    fileContent = stopwordRemover.remove_regularWords(fileContent)
+    fileContent = normalizer.my_normalizing(fileContent)
+    #fileContent = stopwordRemover.remove_proNone(fileContent)
+    #fileContent = stopwordRemover.remove_regularWords(fileContent)
     fileContent = tokenizer.my_word_tokenize(fileContent)
     fileContent = tokenCounter.my_token_conter(fileContent)
     fileContent = dicSorter.dic_key_sort(fileContent, 0)
