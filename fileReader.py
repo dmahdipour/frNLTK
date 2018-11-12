192251909f0e40372fd517ab59579090de6d524c
dmy <daruosh.mehdipour@gmail.com>
2018-11-12 21:47:15 +0330

    Modify fileReader.py to add a def for reading jSon files.

--- fileReader.py
+++ fileReader.py
@@ -1,3 +1,6 @@
+import json
+
+
 def my_file_reader(myFile, fileEncode):
     fileContent = open(myFile, encoding = fileEncode)
     return fileContent.read()
@@ -7,3 +10,9 @@ def my_floatFile_reader(myFile):
     fileContent = open(myFile, 'r')
     x = [float(line.strip()) for line in fileContent if line]    
     return x
+
+
+def my_Json_reader(myFile, fileEncode, keyName):
+    with open(myFile, 'r', encoding = fileEncode) as fp:
+        obj = json.load(fp, strict=False)
+    return obj[keyName]
